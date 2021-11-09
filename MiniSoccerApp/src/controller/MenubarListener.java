faf181f3ed5200ac6bec96f8221fff8e95533dcf
hussain-fatmi <61632279+hussain-fatmi@users.noreply.github.com>
2021-11-09 14:09:52 -0500

    Update MenubarListener.java
    Added javadoc

--- MiniSoccerApp/src/controller/MenubarListener.java
+++ MiniSoccerApp/src/controller/MenubarListener.java
@@ -6,15 +6,30 @@ import view.GamePanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * MenubarListener monitors the menu bar to control the game
+ */
 public class MenubarListener implements ActionListener {
 
+	/**
+	 * The current panel on which the game is displayed
+	 */
 	private final GamePanel gamePanel;
 
+	/**
+	 * Initializes the MenubarListener
+	 * @param panel - the current GamePanel
+	 */
 	public MenubarListener(GamePanel panel) {
 		gamePanel = panel;
 	}
 
-
+	/**
+	 * This class controls the game and panel
+	 * Gives the user the option to play a new game and exit the current game
+	 * The user can pause the game while it is running
+	 * The user can resume the game while it is paused
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		SoccerGame soccerGame = gamePanel.getGame();
