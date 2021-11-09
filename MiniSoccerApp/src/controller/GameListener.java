2063604fb6bea7d2d43961c60b68fc672539fa07
hussain-fatmi <61632279+hussain-fatmi@users.noreply.github.com>
2021-11-09 14:09:02 -0500

    Update GameListener.java
    Added javadoc

--- MiniSoccerApp/src/controller/GameListener.java
+++ MiniSoccerApp/src/controller/GameListener.java
@@ -6,10 +6,21 @@ import view.GamePanel;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+/**
+ * GameListener monitors keystrokes during the soccer game
+ *
+ */
 public class GameListener implements KeyListener {
 
+	/**
+	 * The current panel on which the game is displayed
+	 */
 	private final GamePanel gamePanel;
 
+	/**
+	 * Initializes the GameListener
+	 * @param panel - the current GamePanel
+	 */
 	public GameListener(GamePanel panel) {
 		gamePanel = panel;
 	}
@@ -19,6 +30,10 @@ public class GameListener implements KeyListener {
 
 	}
 
+	/**
+	 * Monitors keys pressed in order to move the GamePlayer controlled by the user
+	 * Moves player up, down, left and right depending on which arrow key is pressed
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		SoccerGame soccerGame = gamePanel.getGame();
